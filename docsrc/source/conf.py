73bf37b4ab8cf71365ad448b9b5cda1bc15139f8
Jonas Liechti <j-i-l@t4d.ch>
2023-12-03 17:39:19 +0100

    started with sphinx

---
+++ docsrc/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Working With Git'
+copyright = '2023, Jonas I. Liechti'
+author = 'Jonas I. Liechti'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/t4d-gmbh/working-with-git",
+    "repository_branch": "main",
+    "path_to_docs": "source/",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "toc_title": "Content",
+    "logo": {
+        "image_light": "_static/Badge_v4_bw.svg",
+        "image_dark": "_static/Badge_v4_wb.svg",
+        }
+}
