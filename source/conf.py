43b66a34727fdbb96e5e1bccd838a6e2101e92c3
Jonas I. Liechti <j-i-l@t4d.ch>
2024-10-07 13:47:00 +0200

    adding extra icons; jinja support

--- source/conf.py
+++ source/conf.py
@@ -1,15 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from setuptools_scm import get_version
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# ###
+# Versioning - get it from the git tag
+# ###
+version: str = get_version(root='..')
+release = version
 
+# ###
+# Basic Project info
+# ###
 project = 'Working With Git'
-copyright = '2023, T4D GmbH'
+copyright = '2023-2024, T4D GmbH'
 author = 'Jonas I. Liechti'
-release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -40,6 +42,7 @@ html_theme_options = {
     "toc_title": "Content",
     "use_sidenotes": True,
     "logo": {
+        "text": "T4D GmbH",
         "image_light": "_static/T4D_logo_bw.svg",
         "image_dark": "_static/T4D_logo_wb.svg",
         "link": "https://t4d.ch",
@@ -48,6 +51,26 @@ html_theme_options = {
     "navigation_with_keys": True,  # Enable keyboard navigation
     "collapse_navigation": False,  # Do not collapse the navigation
     # "sidebar_width": "250px",  # Set the width of the sidebar
+    "icon_links": [
+        {
+            "name": "GitHub Discussion",
+            "url": "https://github.com/t4d-gmbh/working-with-git/discussions",
+            "icon": "fa-regular fa-comments",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Pages",
+            "url": "https://t4d-gmbh.github.io/working-with-git/index.html",
+            "icon": "fa-solid fa-book",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Slides",
+            "url": "https://t4d-gmbh.github.io/working-with-git/slides/index.html",
+            "icon": "fa-solid fa-person-chalkboard",
+            "type": "fontawesome",
+        },
+    ],
 }
 
 favicons = [
@@ -57,3 +80,21 @@ favicons = [
 myst_enable_extensions = [
     "colon_fence",
 ]
+
+# ###
+# Custom jinja parser to enable jinja templating
+# ###
+def rstjinja(app, docname, source):
+    """
+    Render source file with jinja first
+    """
+    # only apply to 'html' builder
+    if app.builder.format == 'html':
+        src = source[0]
+        rendered = app.builder.templates.render_string(
+            src, app.config.html_context
+        )
+        source[0] = rendered
+
+def setup(app):
+    app.connect("source-read", rstjinja)
